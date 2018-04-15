b7feecb0589c37611c1bffd9d3463e5881670caf
Jake Hawken <jake@geocaching.com>
2018-04-15 09:15:19 -0700

    I know you planned it,

--- ex3.py
+++ ex3.py
@@ -16,7 +16,7 @@ print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6.0)
 print("Is it true that 3 + 2 < 5 - 7?")
 
 #prints the arithmetic output
-print(3 + 2 < 5 - 7)
+print(3 + 2 < 5 - 7))
 
 #prints the string, then the arithmetic output
 print("What is 3 + 2?", 3 + 2.0)
@@ -31,5 +31,5 @@ print("How about some more.")
 
 #for each line, prints the string, then the arithmetic output
 print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
+print("Is it greater or equal?", 5 > = -2)
 print("Is it less or equal?", 5 <= -2)
